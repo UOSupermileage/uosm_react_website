c95ff076ddbcce891334e0e9401b5c0f5161520b
cmanage1 <chethinlm@gmail.com>
2022-02-13 10:13:54 -0500

    Fix bracket issue in Theme

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -3,36 +3,31 @@ import { createTheme } from '@mui/material/styles';
 // Bright Grey = "58595b"
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#ec7001',
-        },
-        secondary: {
-            main: "#231f20"
-        }
+  palette: {
+    primary: {
+      main: '#ec7001',
     },
-    components: {
-        MuiTypography: {
-            defaultProps: {
-                variantMapping: {
-                    h1: 'h1',
-                    h2: 'h2',
-                    h3: 'h3',
-                    h4: 'h4',
-                    h5: 'h5',
-                    h6: 'h6',
-                    subtitle1: 'h2',
-                    subtitle2: 'h2',
-                    body1: 'span',
-                    body2: 'span',
-                },
-            },
-        },
-    },
-    secondary: purple,
+    secondary: {
+      main: "#231f20"
+    }
   },
-  status: {
-    danger: orange[500],
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: 'h1',
+          h2: 'h2',
+          h3: 'h3',
+          h4: 'h4',
+          h5: 'h5',
+          h6: 'h6',
+          subtitle1: 'h2',
+          subtitle2: 'h2',
+          body1: 'span',
+          body2: 'span',
+        },
+      },
+    },
   },
 });
 
