9e2b8696faa40e96f649eb665bba77c0c79ebd36
cmanage1 <chethinlm@gmail.com>
2022-02-19 17:19:14 -0500

    Create reusable style const

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,7 +3,6 @@ import { Link } from 'react-scroll';
 import Button from '@mui/material/Button';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-// Use this to make navbar bold
 const navTheme = createTheme({
   palette: {
     primary: {
@@ -15,6 +14,12 @@ const navTheme = createTheme({
   },
 });
 
+const buttonStyle = { 
+  fontWeight: '750',
+  textShadow: '2px 2px 2px #000000',
+  fontSize: '18px',
+};
+
 const Navbar = () => {
   return (
     <ThemeProvider theme={navTheme}>
@@ -24,30 +29,30 @@ const Navbar = () => {
           justifyContent: 'space-around',
         }}
       >
-        <Button sx={{ fontWeight: '750' }}>
+        <Button sx={ buttonStyle }>
           <Link activeClass='active' to='home' spy={true} smooth={true}>
             Home
           </Link>
         </Button>
 
-        <Button sx={{ fontWeight: '750' }}>
+        <Button sx={ buttonStyle }>
           <Link to='welcome' spy={true} smooth={true}>
             Welcome
           </Link>
         </Button>
 
-        <Button sx={{ fontWeight: '750' }}>
+        <Button sx={ buttonStyle }>
           <Link to='team' spy={true} smooth={true}>
             Team
           </Link>
         </Button>
 
-        <Button sx={{ fontWeight: '750' }}>
+        <Button sx={ buttonStyle }>
           <Link to='sponsors' spy={true} smooth={true}>
             Sponsors
           </Link>
         </Button>
-        <Button sx={{ fontWeight: '750' }}>
+        <Button sx={ buttonStyle }>
           <Link to='contact' spy={true} smooth={true}>
             Contact
           </Link>
